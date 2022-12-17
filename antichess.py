c00a06739ea9a6cdd52c8a522211f7b7d34ceaa6
Huang Huang <58092450+StoneDaweiKing@users.noreply.github.com>
2022-12-17 13:01:31 -0500

    Adding legal move judgement for antichess

--- antichess.py
+++ antichess.py
@@ -24,11 +24,30 @@ def make_a_move():
     else:
         uci = input("Black to move: ")
     move = chess.Move.from_uci(uci)
-    if ac_global.board.is_legal(move):
+    if antichess_is_legal(move):
         ac_global.board.push(move)
         print(ac_global.board)
     else:
         raise ValueError("Illegal move.")
+
+
+def antichess_legal_moves():
+    chess_move_list = ac_global.board.pseudo_legal_moves
+    antichess_move_list = []
+    for move in chess_move_list:
+        if ac_global.board.is_capture(move) == True:
+            antichess_move_list.append(move)
+    if not antichess_move_list:
+        antichess_move_list = chess_move_list
+    print(antichess_move_list)
+    return antichess_move_list
+
+def antichess_is_legal(move):
+    if move in antichess_legal_moves():
+        return True
+    else:
+        return False
+    
     
 
 if __name__ == "__main__":
