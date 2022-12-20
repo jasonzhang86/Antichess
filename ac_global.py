687dfa1743798d1f99159e911f7afe9837114a1f
jasonzhang86 <y2785zha@uwaterloo.ca>
2022-12-20 12:30:21 -0500

    implemented minimax strategy

--- ac_global.py
+++ ac_global.py
@@ -6,6 +6,12 @@ opponent = None # one of {WHITE, BLACK}
 
 board = None # The game board
 
+# global constants
+
+INT_MIN = -2147483648
+
+INT_MAX = 2147483647
+
 ####################################################
 
 DEBUG = True # Flag for debug messagess
--- antichess.py
+++ antichess.py
@@ -1,10 +1,10 @@
 import chess
-import random
 import argparse
 
 import ac_global
 from ac_exception import *
 from ac_utils import *
+from ac_search import *
 
 def setup_game():
     """
@@ -26,6 +26,7 @@ def setup_game():
         ac_global.opponent = chess.WHITE
     else:
         raise IllegalPlayerTypeException("Illegal player type [{}]. Please choose one of white and black.".format(args.player))
+
     ac_global.board = chess.Board()
     print_board()
 
@@ -38,49 +39,30 @@ def receive_a_move():
     try:
         uci = input("Waiting for opponent to enter a move: ")
         move = chess.Move.from_uci(uci)
+
         if ac_global.board.is_legal(move):
             print_message("Opponent [{}] moving to {}.".format(chess_color_to_string(ac_global.opponent), uci))
             ac_global.board.push(move)
             print_board()
+
     except ValueError:
         raise IllegalMoveException("Received an illegal move. Please enter again.")
-
-def is_antichess_move(move):
-    return ac_global.board.is_capture(move)
-
-def find_optimal_move(antichess_moves, regular_moves):
-    if antichess_moves:
-        return random.choice(antichess_moves)
-    elif regular_moves:
-        return random.choice(regular_moves)
-    else:
-        raise NoLegalAntichessMoveException("Cannot make a valid antichess move.")
 
 def make_a_move():
     """
     Try to make a move using Minimax strategy and Alpha-Beta pruning.
     """  
-
-    # split all possible moves into antichess/regular moves
-    # :antichess move: moves that capture an opponent's piece, if such a move exists, we must make this move.
-    # :regular move: moves that do not caputure an opponent's piece, we only consider such move if 
-    #                there is no antichess move
-    possible_moves = list(ac_global.board.legal_moves)
-    antichess_moves = []
-    regular_moves = []
-    for move in possible_moves:
-        if is_antichess_move(move):
-            antichess_moves.append(move)
-        else:
-            regular_moves.append(move)
+    alpha = ac_global.INT_MIN
+    beta = ac_global.INT_MAX
 
     # determine an optimal move with the two lists
-    optimal_move = find_optimal_move(antichess_moves, regular_moves)
+    optimal_score, optimal_move = minimax_pruning(board_fen=ac_global.board.fen(), depth=5, alpha=alpha, beta=beta, maximizingPlayer=True)
     print(optimal_move.uci())
     ac_global.board.push(optimal_move)
 
     # For debugging
     print_message("My player [{}] moving to {}.".format(chess_color_to_string(ac_global.my_player), optimal_move.uci()))
+    print_message("My move has score {}.".format(optimal_score))
     print_board()
 
 if __name__ == "__main__":
@@ -95,12 +77,12 @@ if __name__ == "__main__":
         else:
             try:
                 receive_a_move()
-
                 # Game should switch turn after opponent makes a move
                 if ac_global.board.turn == ac_global.opponent:
                     raise IllegalMoveException("Received an illegal move. Please enter again.")
             except IllegalMoveException as e:
                 print(e)
                 print_board()
+
     print_game_result()
     
