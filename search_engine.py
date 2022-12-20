7f0364d6efb7930de92436e6d7b12e150eae6d19
Huang Huang <58092450+StoneDaweiKing@users.noreply.github.com>
2022-12-20 12:40:45 -0500

    Improve the Ai to smash random nerds

--- search_engine.py
+++ search_engine.py
@@ -2,6 +2,20 @@ import evaluation
 import chess
 import valid_move
 import ac_global
+import random
+
+# This is a function meant to order the choices we try at the same level of the game tree
+def order_moves(board, move_list):
+    check_move_list = []
+    remaining_list = []
+    for move in move_list:
+        if board.gives_check(move) == True:
+            check_move_list.append(move)
+        else:
+            remaining_list.append(move)
+    random.shuffle(check_move_list)
+    random.shuffle(remaining_list)
+    return check_move_list + remaining_list
 
 
 def pruning_minimax(board_fen, depth, alpha, beta, current_player, desired_player):
@@ -13,7 +27,9 @@ def pruning_minimax(board_fen, depth, alpha, beta, current_player, desired_playe
     
     if current_player == desired_player:
         maxEval = ac_global.INT_MIN
-        for move in valid_move.antichess_legal_moves(current_board):
+        available_move_list = valid_move.antichess_legal_moves(current_board)
+        improved_move_list = order_moves (current_board, available_move_list)
+        for move in improved_move_list:
             current_board.push(move)
             new_fen = current_board.fen()
             current_board.pop()
@@ -21,7 +37,7 @@ def pruning_minimax(board_fen, depth, alpha, beta, current_player, desired_playe
             if eval >= maxEval:
                 maxEval = eval
                 max_move = move.uci() 
-            alpha = max(alpha, eval)
+            alpha = max(alpha, maxEval)
             if beta <= alpha:
                 break
         return [maxEval, max_move]
@@ -34,7 +50,7 @@ def pruning_minimax(board_fen, depth, alpha, beta, current_player, desired_playe
             current_board.pop()
             eval = pruning_minimax(new_fen, depth-1, alpha, beta, not current_player, desired_player)[0]
             minEval = min(minEval, eval)
-            beta = min(beta, eval)
+            beta = min(beta, minEval)
             if beta <= alpha:
                 break
         return [minEval, '']
